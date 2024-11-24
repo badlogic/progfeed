f3210cca42344904ce5518c84bc4b6230bf083b0
Mario Zechner <badlogicgames@gmail.com>
2024-11-24 03:05:14 +0100

    Jetstream retry logic.

--- src/firehose.ts
+++ src/firehose.ts
@@ -14,23 +14,33 @@ export class Firehose {
 	numEvents = 0
 
 	async run(onPost: (post: FirehosePost) => void, topLevelOnly: boolean = true) {
-		const jetstream = new Jetstream({ ws: WebSocket })
-		jetstream.onCreate("app.bsky.feed.post", event => {
-			const record = event.commit.record as any
-			if (record.text) {
-				if ((topLevelOnly && !record.reply) || !topLevelOnly) {
-					onPost({
-						author: event.did,
-						text: record.text,
-						createdAt: record.createdAt,
-						uri: `at://${event.did}/"app.bsky.feed.post"/${event.commit.rkey}`,
-						cid: event.commit.cid,
-						isReply: record.reply != undefined,
-					})
+		const run = (cursor?: number) => {
+			const jetstream = new Jetstream({ ws: WebSocket, cursor })
+			jetstream.onCreate("app.bsky.feed.post", event => {
+				const record = event.commit.record as any
+				if (record.text) {
+					if ((topLevelOnly && !record.reply) || !topLevelOnly) {
+						onPost({
+							author: event.did,
+							text: record.text,
+							createdAt: record.createdAt,
+							uri: `at://${event.did}/"app.bsky.feed.post"/${event.commit.rkey}`,
+							cid: event.commit.cid,
+							isReply: record.reply != undefined,
+						})
+					}
 				}
-			}
-			this.numEvents++
-		})
-		jetstream.start()
+				this.numEvents++
+			})
+			jetstream.on("error", (error: Error, cursor) => {
+				console.error("Firehose interrupted, retrying in 10 seconds", error)
+				jetstream.close()
+				setTimeout(() => {
+					console.log("Retrying to connect to firehose")
+					run()
+				}, 10000)
+			})
+			jetstream.start()
+		}
 	}
 }
